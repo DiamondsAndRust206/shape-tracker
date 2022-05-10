a9412b1e93c3d76389935a2391f0694de9ec7539
Timothy Gibson <ryaninlux@gmail.com>
2022-05-09 19:48:35 -0700

    Add all dependencies and files.

--- src/index.js
+++ src/index.js
@@ -11,3 +11,7 @@ $(document).ready(function() {
 });
 
 import Triangle from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
